0715c5d2ba9bf7018bf8d097fd0bcc4683b003b8
Frederik <frederiks96@gmail.com>
2016-04-19 20:32:49 +0200

    Rettet getOwner

--- CDIO_Matador/src/main/ChanceCard.java
+++ CDIO_Matador/src/main/ChanceCard.java
@@ -5,7 +5,7 @@ public class ChanceCard {
 	private String text;
 	private boolean isOwnable;
 	private Player owner;
-	
+
 	public ChanceCard(String text){
 		this.text = text;
 		if (text.charAt(0)=='I' || text.charAt(0)=='E') { // Skal lige rettes til, når teksterne er oversat
@@ -15,24 +15,25 @@ public class ChanceCard {
 			isOwnable = false;
 		}
 	}
-	
+
 	public String toString() {
 		return this.text;
 	}
-	
+
 	public boolean isOwnable() {
 		return this.isOwnable;
 	}
-	
+
 	public Player getOwner() {
-		return this.owner;
+		if (isOwnable()) {
+			return this.owner;
+		} else {
+			return null;
+		}
 	}
-	
+
 	public void setOwner(Player player) {
 		this.owner=player;
 	}
 
-
-	
-	// lav kø
 }
