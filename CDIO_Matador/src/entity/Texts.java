07cb8faab0e3d7793f0da7b546992397ba274967
Frederik <frederiks96@gmail.com>
2016-04-19 14:56:49 +0200

    Opdateret CardStack og Texts

--- CDIO_Matador/src/entity/Texts.java
+++ CDIO_Matador/src/entity/Texts.java
@@ -7,10 +7,12 @@ public class Texts {
 	private enum language {Dansk,English};
 	private ResourceBundle rents;
 	private ResourceBundle texts;
+	private ResourceBundle cards;
 	
 	public Texts(language lang) {
 		rents = ResourceBundle.getBundle("properties.rent");
 		texts = ResourceBundle.getBundle("properties."+lang+"_texts");
+		cards = ResourceBundle.getBundle("properties."+lang+"_cards");
 	}	
 	
 	public String getString(String key) {
@@ -19,6 +21,10 @@ public class Texts {
 	
 	public int getRent(String key) {
 		return Integer.parseInt(rents.getString(key));
+	}
+	
+	public String getCardString(String key) {
+		return cards.getString(key);
 	}
 	
 }
