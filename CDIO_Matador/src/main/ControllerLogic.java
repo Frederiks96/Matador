18974272bf2e12e38615be7c909ef35d958b7d8c
Oliver <Oliver.e.t@hotmail.com>
2016-04-19 10:40:58 +0200

    ny main medtode

---
+++ CDIO_Matador/src/main/ControllerLogic.java
@@ -0,0 +1,19 @@
+package main;
+
+public class ControllerLogic {
+	
+	private ControllerGUI c = new ControllerGUI();  
+	
+	
+	public void run() {
+		
+	}
+	
+	public void playerTurn(Player player) {
+		
+	
+		
+		c.closeGUI();
+	}
+
+}
---
+++ CDIO_Matador/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ControllerLogic con = new ControllerLogic();
+		
+		con.run();
+
+	}
+
+}
