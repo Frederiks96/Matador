a17d15b6bf29fb0ad2fcdc008ce9050ce827cd81
Frederik <frederiks96@gmail.com>
2016-04-19 10:04:54 +0200

    DTO

--- CDIO_Matador/src/entity/DTO.java
+++ CDIO_Matador/src/entity/DTO.java
@@ -6,19 +6,21 @@ public interface DTO {
 	
 	// Data Transfer Object - Setters
 	
-	void createPlayer() throws SQLException;
-	void updatePosition()throws SQLException;
-	void setBalance()throws SQLException;
-	void setJailTime()throws SQLException;
-	void setCardId()throws SQLException; // Ikke nødvendig?
-	void setCardPosition()throws SQLException;
-	void setFieldId()throws SQLException; // Ikke nødvendig?
-	void setHouseCount()throws SQLException;
-	void buildHotel()throws SQLException;
-	void mortgage()throws SQLException;
-	void setVehicleID()throws SQLException; // Ikke nødvendig?
-	void setVehicleColour()throws SQLException;
-	void setVehicleType()throws SQLException;
-	void setAccountId()throws SQLException; // Ikke nødvendig?
+	void createPlayer(int id, String name, int position, int jailTime, boolean isActive) throws SQLException;
+	void createAccount(int aId, int balance) throws SQLException;
+	void createVehicle(int vId, String vColour, String vType) throws SQLException;
+	void updatePosition() throws SQLException;
+	void setBalance() throws SQLException;
+	void setJailTime() throws SQLException;
+	void setCardId() throws SQLException; // Ikke nødvendig?
+	void setCardPosition() throws SQLException;
+	void setFieldId() throws SQLException; // Ikke nødvendig?
+	void setHouseCount() throws SQLException;
+	void buildHotel() throws SQLException;
+	void mortgage() throws SQLException;
+	void setVehicleID() throws SQLException; // Ikke nødvendig?
+	void setVehicleColour() throws SQLException;
+	void setVehicleType() throws SQLException;
+	void setAccountId() throws SQLException; // Ikke nødvendig?
 
 }
