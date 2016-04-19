a62d973a48eb55a74414a99958fd3f2f4fd47efb
Oliver <Oliver.e.t@hotmail.com>
2016-04-19 14:46:43 +0200

    tilføjet Field array til GameBoard

--- CDIO_Matador/src/main/GameBoard.java
+++ CDIO_Matador/src/main/GameBoard.java
@@ -1,5 +1,14 @@
 package main;
 
+import fields.AbstractFields;
+
 public class GameBoard {
+
+	private AbstractFields[] LogicFields;
+
+	public AbstractFields getLogicField(int i) {
+
+		return LogicFields[i];
+	}
 
 }
