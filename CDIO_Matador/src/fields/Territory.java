6cce58d08737765c5550b9c6401350357b112e74
Oliver <Oliver.e.t@hotmail.com>
2016-04-20 12:16:10 +0200

    get colour

--- CDIO_Matador/src/fields/Territory.java
+++ CDIO_Matador/src/fields/Territory.java
@@ -8,7 +8,7 @@ import entity.Texts;
 public class Territory extends AbstractFields implements Ownable {
 	
 	ControllerGUI myGUI = new ControllerGUI();
-	GameBoard gameboard = new GameBoard();
+	GameBoard gameboard = new GameBoard(null);
 	private Player owner;
 	private int houseCount;
 	private boolean hasHotel; 
@@ -40,19 +40,28 @@ public class Territory extends AbstractFields implements Ownable {
 	@Override
 	public void landOnField(Player player, Texts text) {
 		if (this.owner==null) {
-			// Ingen ejer
+			String s = myGUI.getUserSelection(text.getFormattedString("buy", this.price), text.getString("Yes"), text.getString("No"));
+			if (s.equals(text.getString("Yes"))) {
+				buyProperty(player);
+			}
 		}
-		else if (this.owner==player) {
-			// Spilleren ejer selv feltet
+		
+		if (!mortgaged && this.owner!=player){
+			
+		}
+		
+		else if (this.owner!=player) {
+			// En anden spiller ejer feltet
+			myGUI.showMessage(text.getFormattedString("rent", rent[houseCount], owner));
 		}
 		else {
-			// En anden spiller ejer feltet
+			// Spilleren ejer selv feltet
 		}
 	}
 	
 
-	public int getRent() {
-		if(allOwned() && houseCount == 0)
+	public int getRent(Player player) {
+		if(player.allOwned() && houseCount == 0)
 			return this.rent[0]*2;
 		else
 			return this.rent[this.houseCount];	
@@ -66,55 +75,45 @@ public class Territory extends AbstractFields implements Ownable {
 	private void buyHouse(){	// housecount betyer hvor mange huse der skal stå på grunden
 		if(owner.getAccount().legalTransaction(-housePrice) && houseCount < 4){
 			myGUI.showMessage(owner.updateBalance(-housePrice));
-			myGUI.setHouse(fieldID, houseCount);
+			myGUI.setHouse(fieldID, houseCount+1);
 			this.houseCount++;
 		}
 	}
 		
 	private void buyHotel(){	// has hotel betyder om der skal sættes eller fjernes hotel
-		if(owner.getAccount().legalTransaction(-housePrice) && !hasHotel){
+		if(owner.getAccount().legalTransaction(-housePrice) && houseCount == 4){
 			myGUI.showMessage(owner.updateBalance(-housePrice));
-			myGUI.setHouse(fieldID, houseCount);
-			hasHotel = true;
+			myGUI.setHotel(fieldID, true);
+			this.houseCount++;
 		}
 	}
+	
+	private void sell
+	
+	private void sellHotel(){
+		if(houseCount == 5){
+			myGUI.showMessage(owner.updateBalance(housePrice/2));
+			myGUI.setHotel(fieldID, false);
+			this.houseCount--;
+		}
+	}
+	
 	
 	public int getHouseCount() {	//unødvendig???
 		return this.houseCount;
 	}
 	
-	public boolean allOwned() {
-		// Returnerer true, hvis en spiller ejer alle felterne, inden for den specifikke farve
-		if(colour.equals("BLUE") || colour.equals("PURPLE")){
-			int j = 0;
-			for(int i = 0; i<40; i++){
-				if(gameboard.getLogicField()[i] instanceof Territory){
-					if (gameboard.getLogicField().getColour() == colour && gameboard.getLogicField().getOwner() == owner ){
-						j++;
-					}
-				}
-			}  
-			return 2 == j;
-		}
-		
-		else{
-			int j;
-			for(int i = 0; i<40; i++){
-				if(gameboard.getLogicField()[i] instanceof Territory){
-					if (gameboard.getLogicField().getColour() == colour && gameboard.getLogicField().getOwner() == owner ){
-						j++;
-					}
-				}
-			}
-			return 3 == j;
-		}		
-	}
+	
 
 	@Override
-	public void buyProperty(Player player) {
-		this.owner = player;
-		myGUI.showMessage(player.updateBalance(-price));
+	public void buyProperty(Player player){
 		
+		if (player.getAccount().legalTransaction(-this.price)){
+			myGUI.showMessage(player.updateBalance(-this.price));
+			this.owner = player;
+			myGUI.setOwner(this.fieldID, player.getName());
+			player.setFleets();
+		}	
 	}
 
 	@Override
@@ -135,10 +134,15 @@ public class Territory extends AbstractFields implements Ownable {
 		return this.owner;
 	}
 
-	private String getColour(){
+	public String getColour(){
 		return colour;
 	}
 
-	
+	@Override
+	public int getRent() {
+		// Unødvendig
+		return 0;
+	}
+
 	
 }
