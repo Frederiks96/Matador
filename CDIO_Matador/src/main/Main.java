7655a92e5fe13f47fc930564eca659d07a951b2b
Frederik <frederiks96@gmail.com>
2016-04-20 13:58:14 +0200

    hej

--- CDIO_Matador/src/main/Main.java
+++ CDIO_Matador/src/main/Main.java
@@ -1,9 +1,10 @@
 package main;
 
+
 public class Main {
 
 	public static void main(String[] args) {
-//		ControllerLogic con = new ControllerLogic();
+		//		ControllerLogic con = new ControllerLogic();
 //		
 //		con.run();
 
