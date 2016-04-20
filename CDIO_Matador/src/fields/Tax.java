e03acddd873187353c2f63c607f3b408e17eee96
Frederik <frederiks96@gmail.com>
2016-04-20 11:29:51 +0200

    Tax done

--- CDIO_Matador/src/fields/Tax.java
+++ CDIO_Matador/src/fields/Tax.java
@@ -18,12 +18,15 @@ public class Tax extends AbstractFields {
 		if (this.fieldID==4) {
 			answer = myGUI.getUserButtonPressed(text.getString("taxOrFine"), text.getString("tax"),text.getString("fine"));
 			if (answer.equals(text.getString("tax"))) {
+				myGUI.showMessage(text.getString("choseTax"));
 				player.updateBalance(-(int)(player.getBalance()*0.1));
 			} else {
+				myGUI.showMessage(text.getString("choseFine"));
 				player.updateBalance(-4000);
 			}
 		} else {
-			myGUI.showMessage(text.getString("land"));
+			myGUI.showMessage(text.getFormattedString("payTax",2000));
+			player.updateBalance(-2000);
 		}
 	}
 
