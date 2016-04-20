8bb38502e8f701da99bd020efc303f8011645d51
Frederik <frederiks96@gmail.com>
2016-04-20 11:21:00 +0200

    Updated tax

--- CDIO_Matador/src/fields/Tax.java
+++ CDIO_Matador/src/fields/Tax.java
@@ -7,6 +7,7 @@ import main.Player;
 public class Tax extends AbstractFields {
 	
 	private ControllerGUI myGUI = new ControllerGUI();
+	private String answer;
 
 	public Tax(int id) {
 		super(id);
@@ -15,8 +16,12 @@ public class Tax extends AbstractFields {
 	@Override
 	public void landOnField(Player player, Texts text) {
 		if (this.fieldID==4) {
-			myGUI.getUserButtonPressed(text.getString("taxOrFine"), text.getString("tax"),text.getString("fine"));
-			
+			answer = myGUI.getUserButtonPressed(text.getString("taxOrFine"), text.getString("tax"),text.getString("fine"));
+			if (answer.equals(text.getString("tax"))) {
+				player.updateBalance(-(int)(player.getBalance()*0.1));
+			} else {
+				player.updateBalance(-4000);
+			}
 		}
 	}
 
