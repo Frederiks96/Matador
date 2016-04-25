176b8dbc9cd392fe692b633436f7948ee374db82
Benjamin Flyger Walther Jensen <Benja.jensen@hotmail.com>
2016-04-25 13:10:57 +0200

    Starten på noget stads.

---
+++ CDIO_Matador/src/Test/TestCasePlayerBuysField.java
@@ -0,0 +1,29 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import entity.GameBoard;
+import entity.Player;
+import entity.fields.AbstractFields;
+import entity.fields.Ownable;
+
+public class TestCasePlayerBuysField {
+
+	private Player player1;
+	private GameBoard board;
+	private AbstractFields[] fields;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		board = new GameBoard();
+
+		
+	}
+	
+	
+}
