7a0d09defad67c3103c792a5610de68e6b50b4e6
Oliver <Oliver.e.t@hotmail.com>
2016-04-27 11:44:48 +0200

    dao

--- CDIO_Matador/src/boundary/DAO.java
+++ CDIO_Matador/src/boundary/DAO.java
@@ -9,22 +9,23 @@ public interface DAO {
 	
 	// Data Acces object - getters
 	
-	int getPosition(Player player) throws SQLException;
+	int getPosition(int playerID) throws SQLException;
 	String getPlayerName(int playerID) throws SQLException;
-	int getBalance(Player player)throws SQLException;
-	
-	int getJailTime()throws SQLException;
+	int getBalance(int playerID)throws SQLException;
+	int getJailTime(int playerID)throws SQLException;
+	String getVehicleColour(int playerID)throws SQLException;
+	String getVehicleType(int playerID)throws SQLException;
 	
 	int getCardId()throws SQLException;
 	int getCardPosition()throws SQLException;
+	
 	
 	int getFieldId()throws SQLException;
 	int getFieldHouseCount(Territory territory)throws SQLException;
 	boolean hasHotel(Territory territory)throws SQLException;
 	boolean isMortgaged(Territory territory)throws SQLException;
 	
-	String getVehicleColour(int playerID)throws SQLException;
-	String getVehicleType(int playerID)throws SQLException;
+	
 	
 
 }
