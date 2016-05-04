6f23d4f7084d3fc1da2e5a2b3b14aa61307682dd
Frederik <frederiks96@gmail.com>
2016-05-04 22:40:40 +0200

    Tilføjet i TerritoryTest

--- CDIO_Matador/src/test/TerritoryTest.java
+++ CDIO_Matador/src/test/TerritoryTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import boundary.GUI_Commands;
-import boundary.SQL;
+import controller.PropertiesController;
 import entity.GameBoard;
 import entity.Player;
 import entity.Texts;
@@ -18,65 +18,161 @@ public class TerritoryTest {
 	private Player player1;
 	private Player player2;
 	private Texts text;
-	private SQL sql;
 	private GUI_Commands gui;
 	private GameBoard board;
-	
+	private PropertiesController manager;
+
 	@Before
 	public void setUp() throws Exception {
 		text = new Texts(language.Dansk);
-		sql = new SQL();
 		gui = new GUI_Commands();
 		player1 = new Player("John", "grøn", "bil");
 		player2 = new Player("Jens", "gul", "bil");
 		board = new GameBoard();
 		board.setupBoard(text);
+		manager = new PropertiesController();
 	}
-	
-	
-	
+
 	@Test
-	public void testBuy(){
+	public void testBuy() {
 		player1.setPosition(1);
 		board.landOnField(player1, text, gui);
-		
+
 		Player expected = this.player1;
 		Player actual = board.getOwner(player1.getPosition());
-		
 		assertEquals(expected, actual);
-		
+
 		int expectedBalance = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getPrice();
 		int actualBalance = player1.getBalance();
-		
 		assertEquals(expectedBalance, actualBalance);
-		}
+	}
 	
 	@Test
-	public void testRent(){
+	public void testMortgage() {
+		player1.setPosition(1);
+		board.landOnField(player1, text, gui);
+		((Ownable)(board.getLogicField(player1.getPosition()))).mortgage(text, gui);
+
+		boolean expected = true;
+		boolean actual = ((Ownable)(board.getLogicField(player1.getPosition()))).isMortgaged();
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testRentNoHouses() {
 		player1.setPosition(1);
 		player2.setPosition(1);
 		board.landOnField(player1, text, gui);
+		board.landOnField(player2, text, gui);
+
+		int expected = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
+		int actual = player2.getBalance();
+		assertEquals(expected, actual);
+
+		expected = 30000+((Ownable)(board.getLogicField(player1.getPosition()))).getRent()-((Ownable)(board.getLogicField(player1.getPosition()))).getPrice();
+		actual = player1.getBalance();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testRentHasAll() {
+		player1.setPosition(1);
+		board.landOnField(player1, text, gui);
+		player1.setPosition(3);
+		board.landOnField(player1, text, gui);
+		player2.setPosition(1);
+		board.landOnField(player2, text, gui);
+
+		int expected = 30000 - 2*((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
+		int actual = player2.getBalance();
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	public void testRentOneHouse() {
+		gui.showMessage("testRentOneHouse");
+		player1.setPosition(1);
+		board.landOnField(player1, text, gui);
+		player1.setPosition(3);
+		board.landOnField(player1, text, gui);
+		manager.manage(gui, player1, text, board); // Buys 1 house
+		
+		player2.setPosition(1);
 		board.landOnField(player2, text, gui);
 		
 		int expected = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
 		int actual = player2.getBalance();
 		assertEquals(expected, actual);
-		
-		expected = 30000 + ((Ownable)(board.getLogicField(player1.getPosition()))).getRent() - ((Ownable)(board.getLogicField(player1.getPosition()))).getPrice();
-		actual = player1.getBalance();
-		assertEquals(expected, actual);
 	}
-	
+
 	@Test
-	public void testMortgage(){
+	public void testRentTwoHouses() {
+		gui.showMessage("testRentTwoHouses");
 		player1.setPosition(1);
 		board.landOnField(player1, text, gui);
-		((Ownable)(board.getLogicField(player1.getPosition()))).mortgage(text, gui);
+		player1.setPosition(3);
+		board.landOnField(player1, text, gui);
+		manager.manage(gui, player1, text, board); // Buys 2 houses
 		
-		boolean expected = true;
-		boolean actual = ((Ownable)(board.getLogicField(player1.getPosition()))).isMortgaged();
+		player2.setPosition(1);
+		board.landOnField(player2, text, gui);
 		
+		int expected = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
+		int actual = player2.getBalance();
 		assertEquals(expected, actual);
 	}
-	
+
+	@Test
+	public void testRentThreeHouses() {
+		gui.showMessage("testRentThreeHouses");
+		player1.setPosition(1);
+		board.landOnField(player1, text, gui);
+		player1.setPosition(3);
+		board.landOnField(player1, text, gui);
+		manager.manage(gui, player1, text, board); // Buys 3 houses
+		
+		player2.setPosition(1);
+		board.landOnField(player2, text, gui);
+		
+		int expected = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
+		int actual = player2.getBalance();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testRentFourHouses() {
+		gui.showMessage("testRentFourHouses");
+		player1.setPosition(1);
+		board.landOnField(player1, text, gui);
+		player1.setPosition(3);
+		board.landOnField(player1, text, gui);
+		manager.manage(gui, player1, text, board); // Buys 4 houses
+		
+		player2.setPosition(1);
+		board.landOnField(player2, text, gui);
+		
+		int expected = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
+		int actual = player2.getBalance();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testRentHotel() {
+		gui.showMessage("testRentHotel");
+		player1.setPosition(1);
+		board.landOnField(player1, text, gui);
+		player1.setPosition(3);
+		board.landOnField(player1, text, gui);
+		manager.manage(gui, player1, text, board); // Buys hotel
+		
+		player2.setPosition(1);
+		board.landOnField(player2, text, gui);
+		
+		int expected = 30000 - ((Ownable)(board.getLogicField(player1.getPosition()))).getRent();
+		int actual = player2.getBalance();
+		assertEquals(expected, actual);
+	}
+
 }
