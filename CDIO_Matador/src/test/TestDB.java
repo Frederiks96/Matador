50a0867c2fb3c8e19f845cd3cf39bd367de7c588
Frederik <frederiks96@gmail.com>
2016-05-06 13:15:20 +0200

    SQL henter nu på foreign key i setBalance

--- CDIO_Matador/src/test/TestDB.java
+++ CDIO_Matador/src/test/TestDB.java
@@ -34,7 +34,7 @@ public class TestDB {
 
 	@AfterClass // Connection lukkes efterfølgende 
 	public static void tearDownAfterClass() throws Exception {
-		sql.dropDataBase(); // Smider databasen, således at testen kan køres flere gange, uden at man får fejl
+		//sql.dropDataBase(); // Smider databasen, således at testen kan køres flere gange, uden at man får fejl
 		sql.closeConnection();
 	}
 
@@ -51,82 +51,96 @@ public class TestDB {
 	public void tearDown() throws Exception {
 	}
 
+//	@Test
+//	public void testCreatePlayer() throws SQLException {
+//		sql.createPlayer(player1);
+//		sql.createPlayer(player2);
+//
+//		// ######################################
+//		//				Test player1
+//		// ######################################
+//
+//		String expectedName = player1.getName();
+//		String expectedColour = player1.getVehicleColour();
+//		String expectedType = player1.getVehicleType();
+//		int expectedBalance = player1.getBalance();
+//		int expectedPosition = player1.getPosition();
+//		int expectedJailTime = player1.getJailTime();
+//		boolean expectedIsAlive = player1.isAlive();
+//		boolean expectedTurn = player1.isTurn();
+//
+//		String actualName = null;
+//		String actualColour = null;
+//		String actualType = null;
+//		int actualBalance = -1;
+//		int actualPosition = -1;
+//		int actualJailTime = -2;
+//		boolean actualIsAlive = false;
+//		boolean actualTurn = true;
+//
+//		actualName = sql.getPlayerName(player1.getPlayerID());
+//		actualColour = sql.getVehicleColour(player1.getPlayerID());
+//		actualType = sql.getVehicleType(player1.getPlayerID());
+//		actualBalance = sql.getBalance(player1.getPlayerID());
+//		actualPosition = sql.getPosition(player1.getPlayerID());
+//		actualJailTime = sql.getJailTime(player1.getPlayerID());
+//		actualIsAlive = sql.getIsAlive(player1.getPlayerID());
+//		actualTurn = sql.getTurn(player1.getPlayerID());
+//
+//		assertEquals(expectedName,actualName);
+//		assertEquals(expectedColour,actualColour);
+//		assertEquals(expectedType,actualType);
+//		assertEquals(expectedBalance,actualBalance);
+//		assertEquals(expectedPosition,actualPosition);
+//		assertEquals(expectedJailTime,actualJailTime);
+//		assertEquals(expectedTurn,actualTurn);
+//		assertEquals(expectedIsAlive,actualIsAlive);
+//
+//		// ######################################
+//		//				Test player2
+//		// ######################################
+//
+//		expectedName = player2.getName();
+//		expectedColour = player2.getVehicleColour();
+//		expectedType = player2.getVehicleType();
+//		expectedBalance = player2.getBalance();
+//		actualBalance = 0;
+//		actualPosition = -1;
+//		actualJailTime = -2;
+//		actualIsAlive = false;
+//		actualTurn = true;
+//
+//		actualName = sql.getPlayerName(player2.getPlayerID());
+//		actualColour = sql.getVehicleColour(player2.getPlayerID());
+//		actualType = sql.getVehicleType(player2.getPlayerID());
+//		actualBalance = sql.getBalance(player2.getPlayerID());
+//		actualPosition = sql.getPosition(player2.getPlayerID());
+//		actualJailTime = sql.getJailTime(player2.getPlayerID());
+//		actualIsAlive = sql.getIsAlive(player2.getPlayerID());
+//		actualTurn = sql.getTurn(player2.getPlayerID());
+//
+//		assertEquals(expectedName,actualName);
+//		assertEquals(expectedColour,actualColour);
+//		assertEquals(expectedType,actualType);
+//		assertEquals(expectedBalance,actualBalance);
+//		assertEquals(expectedPosition,actualPosition);
+//		assertEquals(expectedJailTime,actualJailTime);
+//		assertEquals(expectedTurn,actualTurn);
+//		assertEquals(expectedIsAlive,actualIsAlive);
+//	}
+
 	@Test
-	public void testCreatePlayer() throws SQLException {
-			sql.createPlayer(player1);
-			sql.createPlayer(player2);
-
-		// ######################################
-		//				Test player1
-		// ######################################
-
-		String expectedName = player1.getName();
-		String expectedColour = player1.getVehicleColour();
-		String expectedType = player1.getVehicleType();
-		int expectedBalance = player1.getBalance();
-		int expectedPosition = player1.getPosition();
-		int expectedJailTime = player1.getJailTime();
-		boolean expectedIsAlive = player1.isAlive();
-		boolean expectedTurn = player1.isTurn();
+	public void testSetBalance() throws SQLException {
+		sql.createPlayer(player1);
+		player1.updateBalance(-10);
+		sql.setBalance(player1);
 		
-		String actualName = null;
-		String actualColour = null;
-		String actualType = null;
-		int actualBalance = -1;
-		int actualPosition = -1;
-		int actualJailTime = -2;
-		boolean actualIsAlive = false;
-		boolean actualTurn = true;
-
-			actualName = sql.getPlayerName(player1.getPlayerID());
-			actualColour = sql.getVehicleColour(player1.getPlayerID());
-			actualType = sql.getVehicleType(player1.getPlayerID());
-			actualBalance = sql.getBalance(player1.getPlayerID());
-			actualPosition = sql.getPosition(player1.getPlayerID());
-			actualJailTime = sql.getJailTime(player1.getPlayerID());
-			actualIsAlive = sql.getIsAlive(player1.getPlayerID());
-			actualTurn = sql.getTurn(player1.getPlayerID());
-
-		assertEquals(expectedName,actualName);
-		assertEquals(expectedColour,actualColour);
-		assertEquals(expectedType,actualType);
-		assertEquals(expectedBalance,actualBalance);
-		assertEquals(expectedPosition,actualPosition);
-		assertEquals(expectedJailTime,actualJailTime);
-		assertEquals(expectedTurn,actualTurn);
-		assertEquals(expectedIsAlive,actualIsAlive);
-
-		// ######################################
-		//				Test player2
-		// ######################################
-
-		expectedName = player2.getName();
-		expectedColour = player2.getVehicleColour();
-		expectedType = player2.getVehicleType();
-		expectedBalance = player2.getBalance();
-		actualBalance = 0;
-		actualPosition = -1;
-		actualJailTime = -2;
-		actualIsAlive = false;
-		actualTurn = true;
-
-			actualName = sql.getPlayerName(player2.getPlayerID());
-			actualColour = sql.getVehicleColour(player2.getPlayerID());
-			actualType = sql.getVehicleType(player2.getPlayerID());
-			actualBalance = sql.getBalance(player2.getPlayerID());
-			actualPosition = sql.getPosition(player2.getPlayerID());
-			actualJailTime = sql.getJailTime(player2.getPlayerID());
-			actualIsAlive = sql.getIsAlive(player2.getPlayerID());
-			actualTurn = sql.getTurn(player2.getPlayerID());
-
-		assertEquals(expectedName,actualName);
-		assertEquals(expectedColour,actualColour);
-		assertEquals(expectedType,actualType);
-		assertEquals(expectedBalance,actualBalance);
-		assertEquals(expectedPosition,actualPosition);
-		assertEquals(expectedJailTime,actualJailTime);
-		assertEquals(expectedTurn,actualTurn);
-		assertEquals(expectedIsAlive,actualIsAlive);
+		int expected = player1.getBalance();
+		int actual = 0;
+		
+		actual = sql.getBalance(player1.getPlayerID());
+		
+		assertEquals(expected,actual);
 	}
 
 }
