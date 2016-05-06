78e31d61d242ef4a22a8c870db90c0a289d71e7e
madswerner <madswerner@10.16.169.126>
2016-05-06 15:27:13 +0200

    tax

--- CDIO_Matador/src/test/TaxTest.java
+++ CDIO_Matador/src/test/TaxTest.java
@@ -8,10 +8,10 @@ import entity.GameBoard;
 import entity.Player;
 import entity.Texts;
 import entity.Texts.language;
-import entity.dicecup.DiceCup;
 import entity.fields.Brewery;
+import entity.fields.Tax;
 
-public class taxTest {
+public class TaxTest {
 
 	private Player player1;
 	private Texts text;
@@ -25,14 +25,17 @@ public class taxTest {
 		player1 = new Player("Oliver", "grøn", "bil");
 		board = new GameBoard();
 		board.setupBoard(text);
-
+	
 	}
 	
 	
 	@Test
 	public void payTax10(){
 		player1.setPosition(4);
-		board.landOnField(player, text, gui);
+
+		int actualPlayer1Balance = player1.getBalance();
+		int expectedPlayer1Balance = 30000 - ((Tax)board.getLogicField(4)).landOnField(player1, chosefine, gui, board);
+		
 		
 	}
 
