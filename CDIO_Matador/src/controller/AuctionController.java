f50af9d6dce49c9253a453cab5909ef5e4e469aa
Frederik <frederiks96@gmail.com>
2016-05-07 10:32:03 +0200

    Auction

--- CDIO_Matador/src/controller/AuctionController.java
+++ CDIO_Matador/src/controller/AuctionController.java
@@ -5,21 +5,22 @@ import entity.Texts;
 import entity.fields.AbstractFields;
 import entity.fields.Brewery;
 import entity.fields.Fleet;
+import entity.fields.Ownable;
 import entity.fields.Territory;
 
 public class AuctionController {
 
-	private int currentbid;
+	private int currentBid;
 	private int pass;
 	private int winner;
 
 	public void auction (Player[] players, AbstractFields field, Controller con, GUI_Commands gui, Texts text){
 
-		currentbid = 0;
+		currentBid = 0;
 		pass = 0;
 		winner = -1;
-		
-		
+
+
 		if (legalAuction(field)){
 
 			do {
@@ -29,54 +30,44 @@ public class AuctionController {
 						boolean choice = gui.getUserLeftButtonPressed(text.getFormattedString("bidQuestion", players[i].getName(),
 								field.getName()),text.getString("Yes"),text.getString("No"));
 
-						if (choice){
+						if (choice) {
 							int bid;
 
-							do{
-								bid = gui.getUserInteger(text.getFormattedString("bid",players[i].getName(),currentbid));
+							do {
+								bid = gui.getUserInteger(text.getFormattedString("bid",players[i].getName(),currentBid));
 
 								if (bid > players[i].getBalance()){
 									gui.showMessage(text.getString("failedTransaction"));
 								}
 
-								if (bid <= currentbid){
+								if (bid <= currentBid){
 									gui.showMessage(text.getString("lowBid"));
 								}
-								
-							} while (bid > players[i].getBalance() || bid <= currentbid);
 
-							if (bid > currentbid){
-								currentbid = bid;
+							} while (bid > players[i].getBalance() || bid <= currentBid);
+
+								currentBid = bid;
 								winner = i;
-							}	
-						}else pass++;
+							
+						} else { 
+							pass++;
+						}
 					}
-					if(pass == con.numPlayersAlive()-1)break;
+					if(pass == con.numPlayersAlive()-1) {
+						break;
+					}
 				}
 			} while (pass < con.numPlayersAlive()-1);
 
-			gui.showMessage(text.getFormattedString("bidWinner", players[winner].getName(), field.getName(),currentbid));
-
-
-			//territory
-			if(field instanceof Territory){
-				((Territory) field).setOwner(players[winner], gui);
-				players[winner].updateBalance(-currentbid);
-			}
-			//fleet
-			if (field instanceof Fleet){
-				((Fleet) field).setOwner(players[winner], gui);
-				players[winner].updateBalance(-currentbid);
-			}	
-			//brewery
-			if (field instanceof Brewery){
-				((Brewery) field).setOwner(players[winner], gui);
-				players[winner].updateBalance(-currentbid);
-			}		
+			gui.showMessage(text.getFormattedString("bidWinner", players[winner].getName(), field.getName(),currentBid));
+			
+			((Ownable) field).setOwner(players[winner], gui);
+			players[winner].updateBalance(-currentBid);
 		}
+		// Hvad gør vi, hvis det ikke er en legal auction???
 	}
 
-	
+
 	private boolean legalAuction(AbstractFields field){
 		if (field instanceof Territory){
 			if (((Territory)field).getHouseCount() == 0){
@@ -84,5 +75,5 @@ public class AuctionController {
 			}else return false;
 		}else return true;
 	}
-	
+
 }
