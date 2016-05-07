618f262a3c56589a2d95eeeece7b0d8c85a58534
Frederik <frederiks96@gmail.com>
2016-05-07 14:31:05 +0200

    Properties controller er tættere på at virke

--- CDIO_Matador/src/controller/PropertiesController.java
+++ CDIO_Matador/src/controller/PropertiesController.java
@@ -9,111 +9,99 @@ import entity.fields.Territory;
 import entity.fields.Ownable;
 
 public class PropertiesController {
-	
-	//Kontrolere alt du kan gøre med properties. mangage /mortgage osv. 
+
+	//Kontrollerer alt du kan gøre med properties. mangage /mortgage osv. 
 
 	private AbstractFields property;
 	private String propertyName;
 	private AbstractFields[] fields;
 	private String choice;
 
-	public void manage(GUI_Commands gui, Player player, Texts text, GameBoard gameboard){
+	public void manage(GUI_Commands gui, Player player, Texts text, GameBoard gameboard) {
 
 		fields = gameboard.getLogicFields();
 
-		if(gameboard.getOwnedProperties(player)!=(null)){
+		if (gameboard.getOwnedProperties(player) != null) {
 			propertyName = gui.getUserSelection(text.getString("choosePropertyBuild"),
 					gameboard.getOwnedProperties(player));
 
-			for (int i = 0; i < fields.length; i++) {
-				if(fields[i].getName()==propertyName)
-					property = fields[i];
-			}
+			property = gameboard.getProperty(propertyName);
 
-			do{
 
-				boolean isMortgaged = false;
+			do {
 
-				// Checks if the property is mortgaged
-				for (int j = 0; j < fields.length; j++) {
-					if (fields[j].getName().equals(propertyName)){
-						isMortgaged = ((Ownable)(fields[j])).isMortgaged();
-					}
-				}	
-
-				if(!isMortgaged){
-					choice = gui.getUserButtonPressed("",  text.getString("mortgage"),
+				if (!((Ownable)(property)).isMortgaged()){
+					choice = gui.getUserButtonPressed("", text.getString("mortgage"),
 							text.getString("manageBuildings"), text.getString("back"));
-				}else {
-					choice = gui.getUserButtonPressed("",  text.getString("unMortgage"),
-							text.getString("manageBuildings"), text.getString("back"));
+				} else {
+					choice = gui.getUserButtonPressed("", text.getString("unMortgage"), text.getString("back"));
 				}
 
-				// Build
-				if(choice == text.getString("manageBuildings")){
+				if (choice.equals(text.getString("manageBuildings"))) {
+					// Build
 					build(player, gui, text, gameboard);
+				} else if (choice.equals(text.getString("mortgage"))) {
+					// Mortgage
+					((Ownable)(property)).mortgage(text, gui);
+				} else if (choice.equals(text.getString("unMortgage"))) {
+					// UnMortgage
+					((Territory)(property)).unMortgage();
 				}
 
-				// Mortgage
-				if(choice == text.getString("mortgage")){
-					for (int j = 0; j < fields.length; j++) {
-						if (fields[j].getName().equals(propertyName)){
-							((Territory)(fields[j])).mortgage(text, gui);
-						}
-					}
-				}
-
-				// UnMortgage
-				if(choice == text.getString("unMortgage")){
-					for (int j = 0; j < fields.length; j++) {
-						if (fields[j].getName().equals(propertyName)){
-							((Territory)(fields[j])).unMortgage();
-						}
-					}
-				}
-
-			}while (choice != text.getString("back"));
-		}else gui.showMessage(text.getString("noProperties"));
+			} while (choice != text.getString("back"));
+		} else {
+			gui.showMessage(text.getString("noProperties"));
+		}
 	}
-	private void build(Player player, GUI_Commands gui, Texts text,
-			GameBoard gameboard){ // TODO mangler build even
+
+	private void build(Player player, GUI_Commands gui, Texts text, GameBoard gameboard) { // TODO mangler build even
 		String building;
-		do{
-			building = gui.getUserButtonPressed("",	text.getString("house+"),text.getString("house-"),
-					text.getString("hotel+"),text.getString("hotel-"), text.getString("back"));
+		do {
+			building = gui.getUserButtonPressed("",	text.getString("house+"), text.getString("house-"),
+					text.getString("hotel+"), text.getString("hotel-"), text.getString("back"));
 
 			// Builds a house
-			if (building == text.getString("house+") && gameboard.getHouseCount() < 32 && buildEven()){
-				for (int j = 0; j < fields.length; j++) {
-					if (fields[j].getName().equals(propertyName))
-						((Territory) (fields[j])).buyHouse(text, gui);
-				}	
-			}else gui.showMessage(text.getString("noMoreHouses"));
-
-			// Remove a house
-			if(building == text.getString("house-") && !buildEven()){
-				for (int j = 0; j < fields.length; j++) {
-					if (fields[j].getName().equals(propertyName))
-						((Territory) (fields[j])).sellHouse(gui);
+			if (building.equals(text.getString("house+"))) {
+				if (gameboard.getHouseCount() < 32 && buildEven()) {
+					((Territory)(property)).buyHouse(text, gui);
+				}
+				else if (gameboard.getHouseCount() == 32) { 
+					gui.showMessage(text.getString("noMoreHouses"));
+				} else {
+					gui.showMessage(text.getString("buildEven"));
 				}
 			}
-			// Builds a hotel
-			if(building == text.getString("hotel+") && gameboard.getHotelCount() < 12 && buildEven()){
-				for (int j = 0; j < fields.length; j++) {
-					if (fields[j].getName().equals(propertyName))
-						((Territory) (fields[j])).buyHotel(text, gui);
-				}	
-			}else gui.showMessage(text.getString("noMoreHotels"));
 
-			// Remove a hotel
-			if(building == text.getString("hotel-") && !buildEven()){
-				for (int j = 0; j < fields.length; j++) {
-					if (fields[j].getName().equals(propertyName))
-						((Territory) (fields[j])).sellHotel(gui);
-				}	
+			// Removes a house
+			if(building.equals(text.getString("house-"))) {
+				if (!buildEven()) {
+					((Territory)(property)).sellHouse(gui);
+				} else {
+					gui.showMessage(text.getString("sellEven"));
+				}
 			}
 
-		}while (building != text.getString("back"));
+			// Builds a hotel
+			if(building.equals(text.getString("hotel+"))) {
+				if (gameboard.getHotelCount() < 12 && buildEven()) {
+					((Territory)(property)).buyHotel(text, gui);
+				} else if (gameboard.getHotelCount() == 12) {
+					gui.showMessage(text.getString("noMoreHotels"));
+				} else {
+					gui.showMessage(text.getString("buildEven"));
+				}
+			}
+
+			// Removes a hotel
+			if(building.equals(text.getString("hotel-"))) {
+				if (!buildEven()) {
+					((Territory)(property)).sellHotel(gui);
+				} else {
+					gui.showMessage(text.getString("sellEven"));
+				}
+			}
+
+		} while (building.equals(text.getString("back")));
 	}
 
 	private boolean buildEven(){
@@ -121,14 +109,15 @@ public class PropertiesController {
 		int totalHouse = 0;
 
 		for (int i = 0; i < fields.length; i++) {
-			if(fields[i] instanceof Territory && !property.equals(fields[i]) && ((Territory)fields[i]).getColour() == ((Territory)property).getColour()){
-				if(i==1 || i==3 || i==37 || i==39){
-					totalHouse = ((Territory)fields[i]).getHouseCount();				
-					legal =( totalHouse >= ((Territory)property).getHouseCount());
-				}
-				else{
-					totalHouse += ((Territory)fields[i]).getHouseCount();	
-					legal = ( totalHouse/2 >= ((Territory)property).getHouseCount());
+			if(fields[i] instanceof Territory && !property.equals(fields[i])) {
+				if (((Territory)fields[i]).getColour().equals(((Territory)property).getColour())) {
+					if(i==1 || i==3 || i==37 || i==39) {
+						totalHouse += ((Territory)fields[i]).getHouseCount();				
+						legal = (totalHouse >= ((Territory)property).getHouseCount());
+					} else {
+						totalHouse += ((Territory)fields[i]).getHouseCount();	
+						legal = ( totalHouse/2 >= ((Territory)property).getHouseCount());
+					}
 				}
 			}
 		}
