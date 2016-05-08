7bea7af8d0d41e7d7483ab2d641dc1478ec8c541
madswerner <madswerner@10.16.169.126>
2016-05-08 13:22:22 +0200

    test

--- CDIO_Matador/src/test/SQLTest.java
+++ CDIO_Matador/src/test/SQLTest.java
@@ -21,29 +21,54 @@ public class SQLTest {
 	public void setUp() throws Exception {
 		sql = new SQL();
 		sql.useDB("Hej");
-//		player = new Player("Mads","green","ufo", sql);
+		player = new Player("Mads","green","ufo");
 	}
 
 
-//	@Test 
-//	public void getBalancetest() {
-//		try {
-//			player.updateBalance(-5000);
-//			long start = System.currentTimeMillis();
-//			sql.setBalance(player);
-//			long finish = System.currentTimeMillis();
-//			double result = (finish-start);
-//			System.out.println("The time was: "+result);
-//			
-//			int expected = player.getBalance();
-//			int actual = sql.getBalance(player.getPlayerID());
-//			
-//			assertEquals(expected, actual);
-//			
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	@Test 
+	public void getBalancetest() {
+		try {
+			player.updateBalance(-5000);
+			long start = System.currentTimeMillis();
+			sql.setBalance(player);
+			long finish = System.currentTimeMillis();
+			double result = (finish-start);
+			System.out.println("The time was: "+result);
+			
+			int expected = player.getBalance();
+			int actual = sql.getBalance(player.getPlayerID());
+			
+			assertEquals(expected, actual);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test 
+	public void getPositiontest(){
+		try{
+			player.setPosition(4);
+			long start = System.currentTimeMillis();
+			sql.getPosition(1);
+			long finish = System.currentTimeMillis();
+			double result = (finish-start);
+			System.out.println("The time was: " + result);
+			
+			int expected = player.getPosition();
+			int actual = sql.getPosition(player.getPlayerID());
+			
+			assertEquals(expected, actual);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getJailTime(){
+		
+	}
 	
 	@Test 
 	public void getFromDB() {
