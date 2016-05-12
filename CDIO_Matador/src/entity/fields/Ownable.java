83427929675a359bff61c8b87502bdc76505a228
Frederik <frederiks96@gmail.com>
2016-05-12 12:08:54 +0200

    Ownable

--- CDIO_Matador/src/entity/fields/Ownable.java
+++ CDIO_Matador/src/entity/fields/Ownable.java
@@ -25,13 +25,13 @@ public interface Ownable {
 	/**
 	 * Returns whether a field is owned.
 	 * 
-	 * @return true if the field os owned, otherwise returns null
+	 * @return true if the field is owned, otherwise returns null
 	 */
 	boolean isOwned();
 	
 	/**
 	 * Sets player as the owner of the field in the game logic and the GUI, if the owner has enough credit to buy. 
-	 * Subtracts the price of the field from the players account, otherwise prints a message to the user that he has insufficient funds.
+	 * Subtracts the price of the field from the player's account, otherwise prints a message to the user that he has insufficient funds.
 	 * 
 	 * @param player – The player who wishes to buy the field
 	 * @param text – The text object that specifies which text will be shown to the user, depending on the language
@@ -40,7 +40,7 @@ public interface Ownable {
 	void buyProperty(Player player, Texts text, GUI_Commands gui);
 	
 	/**
-	 * Mortgages a field, and updates the players balance with the value of the mortgage 
+	 * Mortgages a field, and updates the player's balance with the value of the mortgage 
 	 * 
 	 * @param text – The text object that specifies which text will be shown to the user, depending on the language
 	 * @param gui – The GUI object which will display messages to the user
@@ -55,17 +55,35 @@ public interface Ownable {
 	void setMortgage(boolean x);
 	
 	/**
-	 * Cancels the mortgage of a field and subtracts the value of the mortgage and an interest of 10% from the players account.
+	 * Cancels the mortgage of a field and subtracts the value of the mortgage and an interest of 10% from the player's account.
 	 */
 	void unMortgage();
 	
 	/**
 	 * Sells the property to the bank for half the price, and sets the owner to null. Updates the player's balance with the amount. 
 	 * 
-	 * @param player – The owner of the field
+	 * @param player – The owner of the field, who sells the property
 	 */
 	void sellProperty(Player player);
+	
+	/**
+	 * Returns the price of the field
+	 * 
+	 * @return price
+	 */
 	int getPrice();
+	
+	/**
+	 * Returns the current rent of the field, based on the owner and number of properties on the field (if any).
+	 * 
+	 * @param board – The board on which the field is placed
+	 * @return rent
+	 */
 	int getRent(GameBoard board);
+	
+	/**
+	 * Returns whether a field is mortgaged or not.
+	 * @return true if the field is mortgaged, otherwise false.
+	 */
 	boolean isMortgaged();
 }
