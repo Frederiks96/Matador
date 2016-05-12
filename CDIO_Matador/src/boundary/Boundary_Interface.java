455fabe455867bda1619fa5ad0dea19a890d2c1c
Frederik <frederiks96@gmail.com>
2016-05-12 12:25:17 +0200

    DAO og BI

--- CDIO_Matador/src/boundary/Boundary_Interface.java
+++ CDIO_Matador/src/boundary/Boundary_Interface.java
@@ -5,26 +5,117 @@ import desktop_codebehind.Car;
 public interface Boundary_Interface {
 	
 	// User interference
-	
+	/**
+	 * 
+	 * @param message
+	 * @return
+	 */
 	String getUserString(String message);
+	/**
+	 * 
+	 * @param message
+	 * @param buttons
+	 * @return
+	 */
 	String getUserButtonPressed(String message, String... buttons);
+	/**
+	 * 
+	 * @param message
+	 * @param min
+	 * @param max
+	 * @return
+	 */
 	int getUserInteger(String message, int min, int max); // Kan udelades
+	/**
+	 * 
+	 * @param message
+	 * @return
+	 */
 	int getUserInteger(String message);
+	/**
+	 * 
+	 * @param message
+	 * @param options
+	 * @return
+	 */
 	String getUserSelection(String message, String... options);
+	/**
+	 * 
+	 * @param message
+	 * @param trueButton
+	 * @param falseButton
+	 * @return
+	 */
 	boolean getUserLeftButtonPressed(String message, String trueButton, String falseButton);
 	
 	// GUI relateret
+	/**
+	 * 
+	 * @param name
+	 * @param balance
+	 */
 	void addPlayer(String name, int balance);
+	/**
+	 * 
+	 * @param name
+	 * @param balance
+	 * @param car
+	 */
 	void addPlayer(String name, int balance, Car car);
+	/**
+	 * 
+	 * @param message
+	 */
 	void showMessage(String message);
+	/**
+	 * 
+	 */
 	void closeGUI();
+	/**
+	 * 
+	 * @param fieldNumber
+	 * @param name
+	 */
 	void setCar(int fieldNumber, String name);
+	/**
+	 * 
+	 * @param fieldID
+	 * @param houseCount
+	 */
 	void setHouse(int fieldID, int houseCount);
+	/**
+	 * 
+	 * @param fieldID
+	 * @param hasHotel
+	 */
 	void setHotel(int fieldID, boolean hasHotel);
+	/**
+	 * 
+	 * @param id
+	 * @param name
+	 */
 	void setOwner(int id, String name);
+	/**
+	 * 
+	 * @param faceValue1
+	 * @param faceValue2
+	 */
 	void setDice(int faceValue1, int faceValue2);
+	/**
+	 * 
+	 * @param field_id
+	 */
 	void removeOwner(int field_id);
+	/**
+	 * 
+	 * @param position
+	 * @param name
+	 */
 	void removeCar(int position, String name);
+	/**
+	 * 
+	 * @param name
+	 */
 	void removeAllCars(String name);
 	
 }
--- CDIO_Matador/src/boundary/DAO.java
+++ CDIO_Matador/src/boundary/DAO.java
@@ -9,19 +9,106 @@ public interface DAO {
 	
 	// Data Access object - "getters"
 	
+	/**
+	 * 
+	 * @param playerID
+	 * @return
+	 * @throws SQLException
+	 */
 	int getPosition(int playerID) 				throws SQLException;
-	String getPlayerName(int playerID) 			throws SQLException;
-	int getBalance(int playerID) 				throws SQLException;
-	int getJailTime(int playerID) 				throws SQLException;
-	String getVehicleColour(int playerID) 		throws SQLException;
-	String getVehicleType(int playerID) 		throws SQLException;
-	int getOwnerID(int fieldID) 				throws SQLException;
-	int getCardId(int position) 				throws SQLException;
-	int getCardPosition(int cardID) 			throws SQLException;
-	int countPlayers() 							throws SQLException;
-	int getFieldHouseCount(Territory territory) throws SQLException;
-	boolean isMortgaged(AbstractFields field) 	throws SQLException;
-	public String[] getActiveGames() 			throws SQLException;
 	
+	/**
+	 * 
+	 * @param playerID
+	 * @return
+	 * @throws SQLException
+	 */
+	String getPlayerName(int playerID) 			throws SQLException;
+	
+	/**
+	 * 
+	 * @param playerID
+	 * @return
+	 * @throws SQLException
+	 */
+	int getBalance(int playerID) 				throws SQLException;
+	
+	/**
+	 * 
+	 * @param playerID
+	 * @return
+	 * @throws SQLException
+	 */
+	int getJailTime(int playerID) 				throws SQLException;
+	
+	/**
+	 * 
+	 * @param playerID
+	 * @return
+	 * @throws SQLException
+	 */
+	String getVehicleColour(int playerID) 		throws SQLException;
+	
+	/**
+	 * 
+	 * @param playerID
+	 * @return
+	 * @throws SQLException
+	 */
+	String getVehicleType(int playerID) 		throws SQLException;
+	
+	/**
+	 * 
+	 * @param fieldID
+	 * @return
+	 * @throws SQLException
+	 */
+	int getOwnerID(int fieldID) 				throws SQLException;
+	
+	/**
+	 * 
+	 * @param position
+	 * @return
+	 * @throws SQLException
+	 */
+	int getCardId(int position) 				throws SQLException;
+	
+	/**
+	 * 
+	 * @param cardID
+	 * @return
+	 * @throws SQLException
+	 */
+	int getCardPosition(int cardID) 			throws SQLException;
+	
+	/**
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	int countPlayers() 							throws SQLException;
+	
+	/**
+	 * 
+	 * @param territory
+	 * @return
+	 * @throws SQLException
+	 */
+	int getFieldHouseCount(Territory territory) throws SQLException;
+	
+	/**
+	 * 
+	 * @param field
+	 * @return
+	 * @throws SQLException
+	 */
+	boolean isMortgaged(AbstractFields field) 	throws SQLException;
+	
+	/**
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	public String[] getActiveGames() 			throws SQLException;
 
 }
