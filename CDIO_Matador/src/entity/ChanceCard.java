2fb5513bff9c8f00fceb57def64d4010f83f99df
Benjamin Flyger Walther Jensen <Benja.jensen@hotmail.com>
2016-05-12 11:56:48 +0200

    java doc til chance cards

--- CDIO_Matador/src/entity/ChanceCard.java
+++ CDIO_Matador/src/entity/ChanceCard.java
@@ -1,5 +1,10 @@
 package entity;
 
+/**
+ * This class is for the stack of chance cards
+ * @author Benjamin Jensen
+ *
+ */
 public class ChanceCard {
 
 	private String text;
@@ -17,15 +22,23 @@ public class ChanceCard {
 		}
 		this.cardID = cardID;
 	}
-
+/**
+ * @return the string on the card
+ */
 	public String toString() {
 		return this.text;
 	}
-
+/**
+ * 
+ * @return if the card is ownable
+ */
 	public boolean isOwnable() {
 		return this.isOwnable;
 	}
-
+/**
+ * 
+ * @return the owner of the card if the card is ownable otherwise returns null
+ */
 	public Player getOwner() {
 		if (this.isOwnable()) {
 			return this.owner;
@@ -33,11 +46,17 @@ public class ChanceCard {
 			return null;
 		}
 	}
-
+/**
+ * sets a player to be an owner of a chance card
+ * @param player 
+ */
 	public void setOwner(Player player) {
 		this.owner=player;
 	}
-	
+/**
+ * 	
+ * @return the chance cards ID
+ */
 	public int getCardID() {
 		return this.cardID;
 	}
