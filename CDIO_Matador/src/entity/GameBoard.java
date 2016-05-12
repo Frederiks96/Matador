5346307ce916dd081ab4923e9433cc693d8b60f0
madswerner <madswerner@192.168.0.10>
2016-05-12 13:19:16 +0200

    Gameboard done!

--- CDIO_Matador/src/entity/GameBoard.java
+++ CDIO_Matador/src/entity/GameBoard.java
@@ -17,6 +17,12 @@ import entity.fields.Refuge;
 import entity.fields.Tax;
 import entity.fields.Territory;
 
+/**
+ * Class that sets up new or already running gameboard.
+ * @author madswerner
+ *
+ */
+
 public class GameBoard {
 
 	private int hotelCount;
@@ -33,7 +39,13 @@ public class GameBoard {
 	public GameBoard() {
 	}
 
-
+/**
+ * Sets up Fleet, Tax, refuge, chanceField, Brewery & Territory fields on gameboard at 
+ * correct positions.
+ * 
+ * @param text - Information of the field.
+ */
+	
 	public void setupBoard(Texts text) {
 		for  (int i = 0; i < logicFields.length; i++){
 			if (i == 5 || i==15 || i==25 || i==35){
@@ -52,6 +64,15 @@ public class GameBoard {
 		}
 	}
 
+	/**
+	 * Loads a saved gameboard.
+	 * 
+	 * @param players - Sets the games players as owners on fields.
+	 * @param gui - Sets houses and hotels on fields 
+	 * @param sql - Loads all stored information from opened game
+	 * @throws SQLException
+	 */
+	
 	public void loadBoard(Player[] players, GUI_Commands gui, SQL sql) throws SQLException {
 		for  (int i = 0; i < this.logicFields.length; i++) {
 			if (logicFields[i] instanceof Ownable) {
@@ -79,6 +100,14 @@ public class GameBoard {
 			}
 		}
 	}
+	
+	/**
+	 * 
+	 * Returns a string of names of the properties the player owns
+	 * 
+	 * @param player - Owner of property
+	 * @return properties
+	 */
 
 	public ArrayList<String> getOwnedProperties(Player player) {
 		ArrayList<String> properties = new ArrayList<String>();
@@ -93,7 +122,14 @@ public class GameBoard {
 		}
 		return properties;
 	}
-
+	
+	/**
+	 * Returns a string of names of the properties that are not built on.
+	 * 
+	 * @param player - Owner of property
+	 * @return properties
+	 */
+	
 	public ArrayList<String> getOwnedUnbuiltProperties(Player player) {
 		ArrayList<String> properties = new ArrayList<String>();
 		for (int i = 0; i < logicFields.length; i++) {
@@ -113,6 +149,13 @@ public class GameBoard {
 		}
 		return properties;
 	}
+	
+	/**
+	 * Calculates total net-worth of player including balance, properties and buildings.
+	 * 
+	 * @param player 
+	 * @return totalworth
+	 */
 
 	public int netWorth(Player player) {
 		int totalworth = 0;
@@ -135,6 +178,13 @@ public class GameBoard {
 		}
 		return totalworth;
 	}
+	
+	/**
+	 * Calculates amount of buildings on Territory 
+	 * 
+	 * @param sql - loads fields house count from database
+	 * @throws SQLException
+	 */
 
 	public void countBuildings(SQL sql) throws SQLException {
 		for (int i = 0; i<40; i++) {
@@ -150,14 +200,35 @@ public class GameBoard {
 			}
 		}
 	}
+	
+	/**
+	 * Returns the abstract fields object at position i
+	 * 
+	 * @param i - position
+	 * @return logicFields[i]
+	 */
 
 	public AbstractFields getLogicField(int i) {
 		return this.logicFields[i];
 	}
+	
+	/**
+	 * Returns all abstract fields from arraylist
+	 * 
+	 * @return logicFields
+	 */
 
 	public AbstractFields[] getLogicFields() {
 		return this.logicFields;
 	}
+	
+	/**
+	 * Checks if player has all same colored properties
+	 * 
+	 * @param owner - owner of field
+	 * @param COLOUR - color of field
+	 * @return True if he owns all - otherwise false
+	 */
 
 	public boolean hasAll(Player owner, String COLOUR) {
 		int j = 0;
@@ -176,6 +247,13 @@ public class GameBoard {
 		}
 		return j==3;
 	}
+	
+	/**
+	 * Returns abstract field object with input of the properties name
+	 * 
+	 * @param propertyName
+	 * @return logicfields[i]
+	 */
 
 	public AbstractFields getProperty(String propertyName) {
 		for (int i = 0; i < logicFields.length; i++) {
@@ -185,6 +263,13 @@ public class GameBoard {
 		}
 		return null;
 	}
+	
+	/**
+	 * Saves gameboard - Saves the properties that are mortgaged, has a owner or has houses/hotels  
+	 * 
+	 * @param sql - Saves data in database
+	 * @throws SQLException
+	 */
 
 	public void saveBoard(SQL sql) throws SQLException {
 		for(int i = 0; i < 40; i++) {
@@ -201,11 +286,27 @@ public class GameBoard {
 		}
 		deck.updateCards(sql);
 	}
+	
+	/**
+	 * Delegates the chain of events that occurs to a specific field when player lands on it.
+	 * 
+	 * @param player – The player who lands on the field
+	 * @param text – The text object that specifies which texts should be shown to the user through the GUI
+	 * @param gui – The GUI_Commands object, that controls the GUI and shows messages to the user and gets the user's choice 
+	 */
 
 	public void landOnField(Player player, Texts text, GUI_Commands gui) {
 		logicFields[player.getPosition()].landOnField(player, text, gui, this);
 	}
 
+	/**
+	 * Creates deck of cards and shuffles them
+	 * 
+	 * @param text - text that is shown when card is shown
+	 * @param sql - saves deck of cards in database
+	 * @throws SQLException
+	 */
+	
 	public void createCardDeck(Texts text, SQL sql) throws SQLException {
 		deck = new CardStack();
 		deck.newDeck(text);
@@ -213,6 +314,14 @@ public class GameBoard {
 		deck.createCards(sql);
 	}
 
+	/**
+	 * Loads deck of cards
+	 * 
+	 * @param text - loadCards text
+	 * @param sql - loads deck of cards from database
+	 * @throws SQLException
+	 */
+	
 	public void loadCardDeck(Texts text, SQL sql) throws SQLException {
 		deck = new CardStack();
 		try {
@@ -222,38 +331,87 @@ public class GameBoard {
 		}
 
 	}
+	
+	/**
+	 * gets amount of hotels on territory
+	 * 
+	 * @return hotelCount
+	 */
 
 	public int getHotelCount(){
 		return hotelCount;
 	}
+	
+	/**
+	 * Adds one hotel on territory
+	 */
 
 	public void addHotel(){
 		hotelCount++;
 	}
+	
+	/**
+	 * Removes one hotel on territory
+	 */
 
 	public void subtractHotel(){
 		hotelCount--;
 	}
+	
+	/**
+	 * gets amount of houses on territory
+	 * 
+	 * @return houseCount
+	 */
 
 	public int getHouseCount(){
 		return houseCount;
 	}
+	
+	/**
+	 *  Adds one house on territory
+	 */
 
 	public void addHouse(){
 		houseCount++;
 	}
+	
+	/**
+	 *  Removes one house on territory
+	 */
 
 	public void subtractHouse(){
 		houseCount--;
 	}
+	
+	/**
+	 * Gets amount of houses on specific field
+	 * 
+	 * @param field_id
+	 * @return houseCount on field_id
+	 */
 
 	public int getHouseCount(int field_id) {
 		return ((Territory)logicFields[field_id]).getHouseCount();
 	}
+	
+	/**
+	 * Checks if field is owned by any players
+	 * 
+	 * @param field_id - the field we're checking
+	 * @return True if owned
+	 */
 
 	public boolean isOwnable(int field_id) {
 		return this.logicFields[field_id] instanceof Ownable;
 	}
+	
+	/**
+	 * Lets you know who owns a field
+	 * 
+	 * @param field_id - the field we're checking
+	 * @return owner of field or null if there is no owner
+	 */
 
 	public Player getOwner(int field_id) {
 		if (isOwnable(field_id)) {
@@ -261,6 +419,14 @@ public class GameBoard {
 		}
 		return null;
 	}
+	
+	/**
+	 * Sets Player as owner on field
+	 * 
+	 * @param field_id - the field player is owner on
+	 * @param owner - Player who is owner of field
+	 * @param gui - The GUI_Commands object, that controls the GUI and shows messages to the user and gets the user's choice 
+	 */
 
 	public void setOwner(int field_id, Player owner, GUI_Commands gui) {
 		if (isOwnable(field_id)) {
@@ -273,15 +439,36 @@ public class GameBoard {
 			}
 		}
 	}
+	
+	/**
+	 * @return dice cup object
+	 */
 
 	public DiceCup getDiceCup(){
 		return dicecup;
 	}
+	
+	/**
+	 * draws card
+	 * 
+	 * @param player - player who draws card
+	 * @return deck.draw(player)
+	 */
 
 	public String drawCard(Player player){
 		return deck.draw(player);
 
 	}
+	
+	/**
+	 * When player is about to go bankrupt they have the options to mortgage and sell
+	 * houses/hotels
+	 * 
+	 * @param player - Player whom recieves options
+	 * @param debt - The debt the player has
+	 * @param text - The text object that specifies which texts should be shown to the user through the GUI
+	 * @param gui - The GUI_Commands object, that controls the GUI and shows messages to the user and gets the user's choice 
+	 */
 
 	public void probateCourt(Player player, int debt, Texts text, GUI_Commands gui) {
 		gui.showMessage(text.getString("probateCourt"));
@@ -315,6 +502,13 @@ public class GameBoard {
 			}
 		} while (player.getBalance()<debt || button.equals(text.getString("back")));
 	}
+	
+	/**
+	 * Removes all buildings from player
+	 * 
+	 * @param player - player who's buildings are removed
+	 * @param gui - The GUI_Commands object, that controls the GUI and shows messages to the user and gets the user's choice 
+	 */
 
 	private void removeAllBuildings(Player player, GUI_Commands gui){
 		for (int i = 0; i < 40; i++){
@@ -330,6 +524,16 @@ public class GameBoard {
 			}
 		}
 	}
+	
+	/**
+	 * When player hits bankruptcy all his buildings are removed. Besides that, the account
+	 * from players who hit bankruptcy is transfered to creditor.
+	 * 
+	 * @param player - player who goes bankrupt
+	 * @param creditor - player who made other player go bankrupt
+	 * @param gui - The GUI_Commands object, that controls the GUI and shows messages to the user and gets the user's choice
+	 * @param text - The text object that specifies which texts should be shown to the user through the GUI
+	 */
 
 	public void bankrupt(Player player, Player creditor, GUI_Commands gui, Texts text) {
 		player.bankrupt();
@@ -358,11 +562,19 @@ public class GameBoard {
 			}
 		}
 	}
+	
+	/**
+	 * @return all players objects in an array 
+	 */
 
 	public Player[] getPlayers() {
 		return con.getPlayers();
 	}
 
+	/**
+	 * @return Auction controller objects
+	 */
+	
 	public AuctionController getAuctionController() {
 		return con.getAuctionController();
 	}
